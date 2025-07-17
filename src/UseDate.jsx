d8e253e6aef195b1561acb7dadac3e0ad3758bf0
RosyPetrella <petrellamr@gmail.com>
2025-07-17 12:46:03 +0200

    Add UseDate hook and integrate current date display in App component

---
+++ src/UseDate.jsx
@@ -0,0 +1,14 @@
+import { useEffect, useState } from "react";
+export default function UseDate() {
+  const [date, setDate] = useState(new Date());
+
+  useEffect(() => {
+    const timerId = setInterval(() => {
+      setDate(new Date());
+    }, 1000);
+
+    return () => clearInterval(timerId);
+  }, []);
+
+  return date;
+}
